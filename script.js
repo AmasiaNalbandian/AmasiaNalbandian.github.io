7e96589b77b14a4e5b5339516fc368dec612299e
AmasiaNalbandian <analbandian@myseneca.ca>
2021-05-12 10:13:50 -0400

    added overlay text

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
-// load back ass backdrop
-var map = new L.Map('leaflet', {
-	layers: [
-		new L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-			'attribution': 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-		})
-	],
-	center: [40, 20],
-	zoom: 1.5
-});
-
-var lite = L.icon({
-	iconUrl: '../lite.png',
-	iconSize: [20, 20], // size of the icon
-	tooltipAnchor: [0, 0] // points the the tip of the leaf
-});
-
+var map;
+var lite;
 var collection = [];
+
+
 // connects to API to fetch data.
 fetch('https://migrationtechtracker-api.herokuapp.com/api/countries').then((data) => {
 	collection = data.json();
 	return collection;
 }).then((data) => {
+	// load back as backdrop
+	map = new L.Map('leaflet', {
+		layers: [
+			new L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+				'attribution': 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+			})
+		],
+		zoomControl: false,
+		center: [30, -10],
+		zoom: 1.5,
+		minZoom: 1.25,
+		// maxBounds: [
+		// 	//sw
+		// 	[60.712, 94.227],
+		// 	[60.774, -94.125]
+		// ],
+
+	});
+
+
+	// attributes of map.
+	// map.dragging.disable();
+	map.touchZoom.disable();
+	map.doubleClickZoom.disable();
+	// map.scrollWheelZoom.disable();
+	// map.boxZoom.disable();
+	map.keyboard.disable();
+	if (map.tap) map.tap.disable();
+
+	lite = L.icon({
+		iconUrl: '../lite.png',
+		iconSize: [30, 30], // size of the icon
+		tooltipAnchor: [0, 0] // points the the tip of the leaf
+	});
+
+
+
 	collection = data;
+
+
 	// for each country
 	data.forEach((country) => {
 
@@ -30,6 +56,9 @@ fetch('https://migrationtechtracker-api.herokuapp.com/api/countries').then((data
 
 		// for each usecase
 		country.usecase.forEach((usecase) => {
+
+
+
 			if (usecase.organization) {
 				popupText += `<hr/><div id="organization"><b>${usecase.title}</b></div>`
 			}
@@ -38,9 +67,9 @@ fetch('https://migrationtechtracker-api.herokuapp.com/api/countries').then((data
 			<div id="title"><b>${usecase.title}</b> </div>`;
 			}
 			popupText += `
-			<div id="subtitle">Department</div> ${usecase.department}<br/>
-			<div id="subtitle">Details</div> ${usecase.details}<br/>
-			<div id="subtitle">Sources</div>`
+			<div id="info"><div id="subtitle">Department</div> ${usecase.department}</div>
+			<div id="info"><div id="subtitle">Details</div> ${usecase.details}<br/></div>
+			<div id="info"><div id="subtitle">Sources</div></div>`
 
 			usecase.source.forEach((source) => {
 				if (source.link) {
@@ -53,19 +82,17 @@ fetch('https://migrationtechtracker-api.herokuapp.com/api/countries').then((data
 
 		})
 		L.marker([country.latitude, country.longitude], { icon: lite }).addTo(map).bindPopup(popupText);
+
+
+		map.on('click', () => {
+			console.log("clicked");
+			document.getElementById("maptitle").style.display = "none";
+		})
 	})
 
 })
 
 
-// attributes of map.
-// map.dragging.disable();
-map.touchZoom.disable();
-map.doubleClickZoom.disable();
-map.scrollWheelZoom.disable();
-map.boxZoom.disable();
-map.keyboard.disable();
-if (map.tap) map.tap.disable();
 
 
 
